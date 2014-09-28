bca94be7c5fb0d66999b1f72d37011d3b4ab5961
Kevin Lin <kaikai526@gmail.com>
2014-09-27 21:09:34 -0700

    forgot Highlihgt object

--- js/State.js
+++ js/State.js
@@ -1,5 +1,6 @@
-var State = function (offset) {
+var State = function (offset, highlight) {
     this.offset = offset;
+    this.highlight = highlight;
     this.inputs = [];
     this.addInput = function (input) {
         this.inputs.push(input);
@@ -11,4 +12,9 @@ var Input = function (attribute, id, value, type) {
     this.id = id; // gonna be the value of the attribute; so like id=id, or id=name;
     this.value = value;
     this.type = type;
+}
+
+// the words to highlight. note that the word may not be unique..
+var Highlight = function (text) {
+    this.text = text;
 }
